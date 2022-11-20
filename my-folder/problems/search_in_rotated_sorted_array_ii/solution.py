f1b7d486dcecf5a15d17fbc4aa81c7c9dbf123a3
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2022-11-20 00:37:00 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (python3)

---
+++ my-folder/problems/search_in_rotated_sorted_array_ii/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        nums= list(set(nums))
+        begin_index=0
+        end_index= len(nums)-1
+        while (begin_index<= end_index):
+            midpoint= begin_index+(end_index-begin_index)//2
+            midpoint_val= nums[midpoint]
+            if(midpoint_val== target):
+                return True
+            if nums[begin_index]<=nums[midpoint]:
+                if nums[begin_index]<=target and nums[midpoint]>=target:
+                    end_index= midpoint-1
+                else:
+                    begin_index= midpoint+1
+            else:
+                if(nums[midpoint]<=target and nums[end_index]>=target):
+                    begin_index= midpoint+1
+                else:
+                    end_index= midpoint-1
+        return False
+
+
+
+        return False
