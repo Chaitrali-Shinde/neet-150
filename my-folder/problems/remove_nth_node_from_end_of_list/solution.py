5775091f82751ed33a31ce2947e6af80dc76918b
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2022-12-30 04:44:07 +0000

    Sync LeetCode submission - Remove Nth Node From End of List (python3)

--- my-folder/problems/remove_nth_node_from_end_of_list/solution.py
+++ my-folder/problems/remove_nth_node_from_end_of_list/solution.py
@@ -5,27 +5,45 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if(head==None):
-            return head
-        temp= head
-        leni=0
-        while(temp!= None):
-            leni+=1
-            temp=temp.next
-        remove= (leni-n)
-        if(remove==0):
-            temp= head.next
-            #head.next= None
-            return temp
-        curr= head
-        prev=None
-        while(remove>0):
-            prev= curr
-            curr= curr.next
-            remove= remove-1
-        prev.next= curr.next
-        #print(prev.val, curr)
+
+        fast=slow=head
+        while(n>0):
+            fast= fast.next
+            n-=1
+        if not fast:
+            return head.next
+        while fast.next:
+            fast=fast.next
+            slow=slow.next
+        slow.next=slow.next.next
         return head
+        
+
+
+
+        # if(head==None):
+        #     return head
+        # temp= head
+        # leni=0
+        # while(temp!= None):
+        #     leni+=1
+        #     temp=temp.next
+        # remove= (leni-n)
+        # if(remove==0):
+        #     temp= head.next
+        #     #head.next= None
+        #     return temp
+        # curr= head
+        # prev=None
+        # while(remove>0):
+        #     prev= curr
+        #     curr= curr.next
+        #     remove= remove-1
+        # prev.next= curr.next
+        # #print(prev.val, curr)
+        # return head
+        
+
 
 
 
