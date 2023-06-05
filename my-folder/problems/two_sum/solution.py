5b70f0ce9c0e2623e8191db43147fedfa185b87b
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-05 18:51:25 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result=[]
-        for i in range(0, len(nums)):
-            ans= target-nums[i]
-            temp=nums[i]
-            nums[i]=-1
-            if ans in nums and nums[i]!=ans:
-                result.append(i)
-                result.append(nums.index(ans))
-                return result
-            nums[i]=temp
-        return result
+        d = {}
+        for i, j in enumerate(nums):
+            r = target - j
+            #print(d)
+            if r in d: 
+                return [d[r], i]
+            d[j] = i
+        
