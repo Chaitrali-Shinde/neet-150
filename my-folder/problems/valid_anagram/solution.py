d800029aebd2b0596e313e51339d398c3b7882e4
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-05 03:05:53 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- my-folder/problems/valid_anagram/solution.py
+++ my-folder/problems/valid_anagram/solution.py
@@ -1,13 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if(len(s)!= len(t)):
+        # approach 1
+        # s= list(s)
+        # t= list(t)
+
+        # s.sort()
+        # t.sort()
+        # if s ==t:
+        #     return True
+        # return False
+        # approach 2
+        # return sorted(t)== sorted(s)
+
+        # approach3
+        countS, countT={}, {}
+        if len(s)!= len(t): return False
+
+        for i in range(len(s)):
+            countS[s[i]]= 1+ countS.get(s[i], 0)
+            countT[t[i]]= 1+ countT.get(t[i], 0)
+        if countT!= countS:
             return False
-        char1= list(s)
-        char2= list(t)
-        for i in char1:
-            if(i in char2):
-                j= char2.index(i)
-                char2.pop(j)
-            else:
-                return False
         return True
+            
