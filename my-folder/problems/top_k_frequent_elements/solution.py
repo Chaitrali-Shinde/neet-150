0537386d0f20feeab6eca7df352bca6dce5fe280
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-17 05:37:20 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -1,26 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # dic= {}
-        # for i in nums:
-        #     dic[i]= dic.get(i, 0)+1
-        # result=[]
-        # myKeys = list(dic.values())
-        # myKeys.sort()
-        # sorted_dict = {i: dic[i] for i in myKeys}
-        # res= list(sorted_dict.keys())
-        # for i in range(0, k):
-        #     result.append(res[i])
+        
 
+        #we are using bucket sort logic here
+        #we have created a bucket of for [[], [], [], ..., []]
+        # index indicates the number of occurance which is not greater than the size of the nums, and the value at that index indicates the actual numbers who have that occurance(index no. of occurance)
         bucket= [[] for i in range(len(nums)+1)]
         mydic= {}
-
+        
+        #1-->3, 2-->2, 3-->1
         for i in nums:
             mydic[i]= mydic.get(i, 0)+1
         result=[]
-
+        #bucket index represent occurance
         for key, val in mydic.items():
             bucket[val].append(key)
-        #print(bucket)
+        print(bucket)
 
         for i in range(len(bucket)-1, 0, -1):
             for n in bucket[i]:
