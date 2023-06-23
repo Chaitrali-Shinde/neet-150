2bee60a84607ed117b57b43b7e5df1c298262246
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-23 18:23:55 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i, j in enumerate(nums):
             r = target - j
