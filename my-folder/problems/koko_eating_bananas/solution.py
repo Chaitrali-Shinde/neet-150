bfe532d1236120db46c7302a302700a614fb8daf
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-25 18:19:26 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- my-folder/problems/koko_eating_bananas/solution.py
+++ my-folder/problems/koko_eating_bananas/solution.py
@@ -1,20 +1,17 @@
 import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        leftK=1
-        rightK= max(piles)
-        result=[]
-        while(leftK<=rightK):
-            midK=(leftK+rightK)//2
+        l, r=1, max(piles)
+        res=r
+        while l<=r:
+            k= (l+r)//2
             hours=0
-            for i in piles:
-                hours+= i//midK
-                if(i%midK!=0):
-                    hours+=1
-        
-            if hours<=h:
-                rightK= midK-1
-            else:
-                leftK= midK+1
-    
-        return leftK
+            for p in piles:
+                hours+=math.ceil(p/k)
+            if hours>h:
+                l= k+1
+            else :
+                res= min(res, k)
+                r=k-1
+
+        return res
