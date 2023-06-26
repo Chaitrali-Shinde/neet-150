4598f662ccb555e64c6fe2a5e38887c693762670
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-06-26 21:49:34 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (python3)

--- my-folder/problems/search_in_rotated_sorted_array/solution.py
+++ my-folder/problems/search_in_rotated_sorted_array/solution.py
@@ -1,23 +1,54 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        begin_index=0
-        end_index= len(nums)-1
-        while (begin_index<= end_index):
-            midpoint= begin_index+(end_index-begin_index)//2
-            midpoint_val= nums[midpoint]
-            if(midpoint_val== target):
-                return midpoint
-            if nums[begin_index]<=nums[midpoint]:
-                if nums[begin_index]<=target and nums[midpoint]>=target:
-                    end_index= midpoint-1
+        l, r= 0,len(nums)-1
+        while l<=r:
+            mid= (l+r)//2
+            if target== nums[mid]:
+                return mid
+            
+            #left sorted portion
+            if nums[l]<=nums[mid]:
+                if target< nums[l] or target> nums[mid]:
+                    l= mid+1
+                    
                 else:
-                    begin_index= midpoint+1
+                   r= mid-1
+            
+            #right sorted portion
             else:
-                if(nums[midpoint]<=target and nums[end_index]>=target):
-                    begin_index= midpoint+1
+                if target<nums[mid] or target >nums[r]:
+                    r= mid-1
                 else:
-                    end_index= midpoint-1
+                    l= mid+1 
+        
         return -1
+                
+
+
+
+
+
+
+
+
+
+
+        # while (l<= r):
+        #     mid= l+(r-l)//2
+        #     mid_val= nums[mid]
+        #     if(mid_val== target):
+        #         return mid
+        #     if nums[l]<=nums[mid]:
+        #         if nums[l]<=target and nums[mid]>=target:
+        #             r= mid-1
+        #         else:
+        #             l= mid+1
+        #     else:
+        #         if(nums[mid]<=target and nums[r]>=target):
+        #             l= mid+1
+        #         else:
+        #             r= mid-1
+        # return -1
 
 
 
