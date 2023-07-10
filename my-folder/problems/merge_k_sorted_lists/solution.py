5eb8b1fe09b80ba456d8fb63a1b1848d1a0f9e6c
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2023-07-10 22:52:18 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- my-folder/problems/merge_k_sorted_lists/solution.py
+++ my-folder/problems/merge_k_sorted_lists/solution.py
@@ -6,21 +6,67 @@
 class Solution:
     
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        node= ListNode()
-        nums=[]
-        for i in lists:
-            print(i)
-            while i!= None:
-                print(i.val)
-                nums.append(i.val)
-                i= i.next
-            
-        nums.sort()
-        curr= node
-        for i in nums:
-            new_node= ListNode(i)
-            while(curr.next!= None):
-                curr= curr.next
-            curr.next= new_node
+        # node= ListNode()
+        # nums=[]
+        # for i in lists:
+        #     #print(i)
+        #     while i!= None:
+        #         #print(i.val)
+        #         nums.append(i.val)
+        #         i= i.next
+        # #print(nums)
+        # nums.sort()
+        # curr= node
+        # for i in nums:
+        #     new_node= ListNode(i)
+        #     while(curr.next!= None):
+        #         curr= curr.next
+        #     curr.next= new_node
 
-        return node.next
+        # return node.next
+
+        if not lists or len(lists)== 0:
+            return None
+
+        while len(lists)>1:
+
+            mergedList=[]
+            for i in range( 0, len(lists), 2):
+                l1= lists[i]
+                l2= lists[i+1] if (i+1)<len(lists) else None
+                mergedList.append(self.merge_lists(l1, l2))
+            lists= mergedList
+        
+        return lists[0]
+
+    def merge_lists(self, list1, list2):
+        dummy= ListNode()
+        tail= dummy
+
+        while list1 and list2:
+            if list1.val<list2.val:
+                tail.next=list1
+                list1= list1.next
+
+            else:
+                tail.next= list2
+                list2= list2.next
+            tail= tail.next
+
+        if list1:
+            tail.next= list1
+        if list2:
+            tail.next= list2
+
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
